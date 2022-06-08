0eb223b1a0bc0c1f58f4873eec42f8829ba32511
yoonbigbear <yoonbigbear@hotmail.com>
2022-06-09 01:05:34 +0900

    유니티 패킷통신 완료. 비동기 데이터 받을 준비중 (유니태스크 활용)

--- client2/Client2_1/Assets/Connector.cs
+++ client2/Client2_1/Assets/Connector.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using UnityEngine;
 
 public class Connector
--- client2/Client2_1/Assets/Network.cs
+++ client2/Client2_1/Assets/Network.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
+using FlatBuffers;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Networking;
 
 public class Network : MonoBehaviour
 {
@@ -13,15 +16,27 @@ public class Network : MonoBehaviour
     static IPEndPoint endPoint = new IPEndPoint(ipAddr, 11000);
     static Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
+    Dictionary<uint, GameObject> objs = new Dictionary<uint, GameObject>();
+
+    DateTime start;
+    DateTime now;
+
     // Start is called before the first frame update
     void Start()
     {
         Connector connector = new Connector();
-    }
+        socket.Connect(endPoint);
 
-    // Update is called once per frame
-    void Update()
-    {
+        FlatBufferBuilder builder = new FlatBufferBuilder(1024);
+        var id = builder.CreateString("sampleId2");
+        var password = builder.CreateString("samplepassword2");
+        account.LoginReq.StartLoginReq(builder);
+        account.LoginReq.AddId(builder, id);
+        account.LoginReq.AddPw(builder, password);
+        var acc = account.LoginReq.EndLoginReq(builder);
+        builder.Finish(acc.Value);
+        var body = builder.SizedByteArray();
+
         //헤더 전송
         byte[] sendBuf = new byte[(sizeof(ushort) + sizeof(ushort))];
         Array.Copy(BitConverter.GetBytes((ushort)Protocol.LoginReq), 0, sendBuf, 0, sizeof(ushort));
@@ -32,5 +47,142 @@ public class Network : MonoBehaviour
         byte[] pkt_body = new byte[body.Length];
         Array.Copy(body, 0, pkt_body, 0, body.Length);
         socket.Send(pkt_body);
+
+        StartCoroutine(Packetreceive());
     }
-}
+
+	IEnumerator Packetreceive()
+	{
+        start = DateTime.Now;
+        now = DateTime.Now;
+
+        while (true)
+        {
+            byte[] recvbuf = new byte[4];
+            socket.Receive(recvbuf);
+            var prot = BitConverter.ToUInt16(recvbuf, 0);
+            var size = BitConverter.ToUInt16(recvbuf, 2);
+
+            Protocol rceived_id = (Protocol)prot;
+            Console.WriteLine($"{rceived_id.ToString()}");
+
+            byte[] recv = new byte[size];
+            socket.Receive(recv);
+            var recvBuf = new ByteBuffer(recv);
+
+
+
+            switch (rceived_id)
+            {
+                case Protocol.LoginAck:
+                    {
+                        var wor = account.LoginAck.GetRootAsLoginAck(recvBuf);
+                        for (int i = 0; i < wor.CharactersLength; ++i)
+                        {
+                            var datas = wor.Characters(i).Value;
+                            Console.WriteLine($"{datas.CharId} {datas.Nickname} {datas.JobClass} {datas.CharId}");
+                        }
+
+                        FlatBufferBuilder builder = new FlatBufferBuilder(1024);
+                        account.SelectCharacterReq.StartSelectCharacterReq(builder);
+                        account.SelectCharacterReq.AddCharId(builder, 484280193545015295);
+                        var b = account.SelectCharacterReq.EndSelectCharacterReq(builder);
+                        builder.Finish(b.Value);
+                        var body = builder.SizedByteArray();
+
+                        //헤더 전송
+                        byte[] sendBuf = new byte[(sizeof(ushort) + sizeof(ushort))];
+                        Array.Copy(BitConverter.GetBytes((ushort)Protocol.SelectCharacterReq), 0, sendBuf, 0, sizeof(ushort));
+                        Array.Copy(BitConverter.GetBytes((ushort)body.Length), 0, sendBuf, sizeof(ushort), sizeof(ushort));
+                        socket.Send(sendBuf);
+
+                        //body 전송
+                        byte[] pkt_body = new byte[body.Length];
+                        Array.Copy(body, 0, pkt_body, 0, body.Length);
+                        socket.Send(pkt_body);
+                    }
+                    break;
+                case Protocol.SelectCharacterAck:
+                    {
+                        var wor = account.SelectCharacterAck.GetRootAsSelectCharacterAck(recvBuf);
+
+                        Console.WriteLine($"{wor.Position.Value.X}  {wor.Position.Value.Y}  {wor.Position.Value.Z}");
+                    }
+                    break;
+                case Protocol.MoveStartSync:
+                    {
+                        var wor = world.MoveStartSync.GetRootAsMoveStartSync(recvBuf);
+                        uint objid = wor.ObjId;
+                        var dir = wor.Dir.Value;
+                        var spd = wor.Speed;
+                        //Console.WriteLine($"{objid}  {dir.Value.X}  {dir.Value.Y} {spd}");
+                        var obj = objs[objid];
+                        obj.spd = spd;
+                        obj.move = true;
+
+                        obj.d.x = dir.X;
+                        obj.d.y = dir.Y;
+                    }
+                    break;
+                case Protocol.EnterFieldSync:
+                    {
+                        var wor = world.EnterFieldSync.GetRootAsEnterFieldSync(recvBuf);
+                        for (int i = 0; i < wor.ObjIdLength; ++i)
+                        {
+                            var objid = wor.ObjId(i);
+                            var pos = wor.Pos(i);
+                            Debug.Log($" 입장 {objid} {pos.Value.X} {pos.Value.Y} {pos.Value.Z}");
+
+                            GameObject obj = new GameObject();
+                            obj.v.x = pos.Value.X;
+                            obj.v.y = pos.Value.Y;
+                            obj.v.z = pos.Value.Z;
+
+                            objs.TryAdd(objid, obj);
+                        }
+                    }
+                    break;
+                case Protocol.MoveStopSync:
+                    {
+                        var wor = world.MoveStopSync.GetRootAsMoveStopSync(recvBuf);
+                        var objid = wor.ObjId;
+                        var pos = wor.Pos;
+                        var obj = objs[objid];
+
+                        Debug.Log($" 도착 {objid} {pos.Value.X} {pos.Value.Y} {pos.Value.Z}");
+
+                        obj.d.x = 0;
+                        obj.d.y = 0;
+                        obj.move = false;
+                    }
+                    break;
+            }
+            yield return null;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        now = DateTime.Now;
+        var interval = now - start;
+        start = now;
+        float dt = (float)interval.TotalMilliseconds;
+
+        foreach (var obj in objs)
+        {
+            var e = obj.Value;
+            if (e.move)
+            {
+                e.v.x = e.v.x + (dt * e.spd * e.d.x);
+                e.v.y = e.v.y + (dt * e.spd * e.d.y);
+            }
+        }
+    }
+
+    UniTask<bool> GetTextAsync()
+    {
+        SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+        byte[] recvbuf = new byte[4];
+        return socket.ReceiveAsync(args);
+    }
+
